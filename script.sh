63cf2f8e3c9b581965eea77a25f5e62c8eab48dc
Ramon Siqueira <152916035+Rmns28@users.noreply.github.com>
2025-09-10 00:42:49 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "🚀 Iniciando a configuração da infraestrutura..."
+
+# Criando grupos
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+# Criando diretórios
+mkdir /adm
+mkdir /ven
+mkdir /sec
+mkdir /publico
+
+# Definindo permissões dos diretórios
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+# Criando usuários e atribuindo aos grupos
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "✅ Infraestrutura criada com sucesso!"
